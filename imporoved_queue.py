0bd6942180bc3309e4237b2100da25242c3e87d0
Abdelrahman A. Saad <abdelrahman.a171@gmail.com>
2023-08-22 18:58:29 +0300

    adding exception for conversion error and fix class method improved queue

--- imporoved_queue.py
+++ imporoved_queue.py
@@ -5,22 +5,31 @@ class QueueOutOfRangeException(Exception):
     pass
 
 
+class ConversionError(Exception):
+    pass
+
+
 class ImprovedQueue(q.LQueue):
     queues_dict = {}
 
     def __init__(self, size, name):
         super().__init__()
         self.name = name
-        self.size = size
+        if isinstance(size, int):
+            self.size = size
+        else:
+            raise ConversionError(size, type(size), "size should be of type int")
+
         self.queues_dict[self.name] = {"size": self.size, "data": self.lqueue}
 
     def insert(self, item):
-        if len(self.lqueue) <= self.size:
+        if len(self.lqueue) < self.size:
             super().insert(item)
         else:
             raise QueueOutOfRangeException(self.size)
 
-    def get_queue(self, name):
-        if self.queues_dict[name]:
-            return self.queues_dict[name]
+    @classmethod
+    def get_queue(cls, name):
+        if cls.queues_dict[name]:
+            return cls.queues_dict[name]
         return None
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import queue as q
+import imporoved_queue as IQ
 
 
 def main():
 
-    q1 = q.LQueue()
+    q1 = IQ.ImprovedQueue(10, 'first queue')
+    # q1 = IQ.ImprovedQueue('abc', 'first queue')
 
     for i in range(10):
         q1.insert(i)
 
+    search_queue = IQ.ImprovedQueue.get_queue('first queue')
+    print(search_queue)
+    print(search_queue['data'])
     for i in range(10):
         print(q1.pop())
 
